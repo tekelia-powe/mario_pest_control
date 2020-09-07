68d4c75ed58c657331f526270e251a7b41dfe964
tekelia-powe <tekelia.powe@bryanuniversity.edu>
2020-09-06 21:43:35 -0500

    Updates

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let total = 0;
+
+document.getElementById("one").addEventListener("input", myFunction);
+document.getElementById("two").addEventListener("input", myFunction);
+document.getElementById("three").addEventListener("input", myFunction);
+
+function myFunction() {
+    const num1 = document.getElementById("one").value * 5;
+    const num2 = document.getElementById("two").value * 7;
+    const num3 = document.getElementById("three").value * 11;
+    
+    var total = parseInt(num1) + parseInt(num2) + parseInt(num3);
+    document.getElementById("total").value = total+" Coins";
+
+  
+}
